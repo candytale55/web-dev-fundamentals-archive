1bd4bec68af0a3c424fa6c8efa63e31dc0caf17d
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2019-12-02 20:23:09 +0100

    Check common mistakes

--- library.js
+++ library.js
@@ -13,8 +13,6 @@ const store = {
   }
 };
 
-
-
 const checkInventory = (order) => {
   return new Promise ((resolve, reject) => {
    setTimeout(()=> {  
@@ -34,11 +32,6 @@ const checkInventory = (order) => {
 }, generateRandomDelay());
  });
 };
-/*
-checkInventory() expects an order argument and returns a promise. If there are enough items in stock to fill the order, the promise will resolve to an array. The first element in the resolved value array will be the same order and the second element will be the total cost of the order as a number.
-*/
-
-
 
 const processPayment = (responseArray) => {
   const order = responseArray[0];
@@ -59,10 +52,6 @@ const processPayment = (responseArray) => {
 }, generateRandomDelay());
  });
 };
-/*
-processPayment() expects an array argument with the order as the first element and the purchase total as the second. This function returns a promise. If there is a large enough balance on the giftcard associated with the order, it will resolve to an array. The first element in the resolved value array will be the same order and the second element will be a tracking number.
-*/
-
 
 
 const shipOrder = (responseArray) => {
@@ -74,10 +63,6 @@ const shipOrder = (responseArray) => {
 }, generateRandomDelay());
  });
 };
-/*
-shipOrder() expects an array argument with the order as the first element and a tracking number as the second. It returns a promise which resolves to a string confirming the order has shipped.
-*/
-
 
 
 // This function generates a random number to serve as a "tracking number" on the shipping label. In real life this wouldn't be a random number
