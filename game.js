8106aee447f88114fc88dbfb02db154eece214ff
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2020-02-20 07:04:09 +0100

    First iteration of the whole game

---
+++ game.js
@@ -0,0 +1,18 @@
+// The game.js file contains the game’s settings
+
+const config = {
+  type: Phaser.AUTO,
+  width: 480,
+  height: 640,
+  backgroundColor: '#808080',
+  scene: [StartScene, GameScene, EndScene],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: 0,
+      enableBody: true,
+    }
+  },
+};
+
+const game = new Phaser.Game(config);
