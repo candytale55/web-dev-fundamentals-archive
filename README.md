604d2c76e4f5fcf1e6144bebb60f5733fcf8a816
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2020-03-03 21:36:47 +0100

    Third commit - Added html instructions section and CSS for styling. Re-did the function randomChoreDoorGenerator and the if-else loop inside it.

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 JS, html & css single-page website with fully-functional game. Three doors, you have to chose and open one, and you lose if you get the chore bot on the first or second try. You win if you get to the third.
 
+Third commit - Added html instructions section and CSS for styling. Re-did the function randomChoreDoorGenerator and the if-else loop inside it. 
 
+There's an ERROR : The img are not loading correctly (broken image). I already tested using the relative paths directly in randomChoreDoorGenerator and the results are the same. It works when the variable e.g. spaceDoorPath are used directly, but not when it is assigned inside randomChoreDoorGenerator. 
--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let doorImage2 = document.getElementById('door2');
 let doorImage3 = document.getElementById('door3');
 
 // variables containing a string with the ChoreBot, Beach and Space Door paths.
-let botDoorPath = "img/robot.svg"; 
-let beachDoorPath = "img/beach.svg";
-let spaceDoorPath = "img/space.svg";
+const botDoorPath = "img/robot.svg"; 
+const beachDoorPath = "img/beach.svg";
+const spaceDoorPath = "img/space.svg";
 
 const numClosedDoors = 3; // amount of doors in the game
 
@@ -20,32 +20,48 @@ let openDoor1, openDoor2, openDoor3 ; //They’ll be assigned values within rand
 
 // doorImage1.onclick run when door images are clicked. 
 doorImage1.onclick = () => {
-  doorImage1.src = botDoorPath;   // when you click on the door, the closed door changes to the ChoreBot
+  doorImage1.src = openDoor1;   // when you click on the door, the closed door changes to the ChoreBot or to the Space or Beach landscapes.
 };
-
+///////// ERROR : The img are not loading. I already tested using the relative paths directly in randomChoreDoorGenerator and the results are the same. It works when the variable e.g. spaceDoorPath are used directly, but not when it is assigned inside randomChoreDoorGenerator. 
 doorImage2.onclick = () => {
-  doorImage2.src= beachDoorPath;  
+  doorImage2.src= spaceDoorPath;  
 };
 
 doorImage3.onclick = () => {
-  doorImage3.src = spaceDoorPath;
+  doorImage3.src = openDoor3;
 };
 
+
+
   
-  
-  /* NOT WORKING - IF I UNCOMMENT, THE DOORS CAN'T BE CHOSEN
-  // randomly generates the door that hides the ChoreBot.
-  const randomChoreDoorGenerator = () => {
-      let choreDoor = Math.floor(Math.random() * numClosedDoor; // Will randomly generate a whole number between 0 and 2.
+  // generates the door that hides the ChoreBot.
+const randomChoreDoorGenerator = () => {
+  const choreDoor = Math.floor(Math.random() * numClosedDoor); // Will randomly generate a whole number between 0 and 2. 
 
-      if (choreDoor === 0) {
+  if (choreDoor === 0) {
+    openDoor1 = botDoorPath;
+    openDoor2 = spaceDoorPath;
+    openDoor3 = beachDoorPath;
 
-        } else if (choreDoor === 1){
-    
-        } else { // choreDoor === 2 }
-        
-  };
+  } else if (choreDoor === 1){
+    openDoor2 = botDoorPath;
+    openDoor1 = spaceDoorPath;
+    openDoor3 = beachDoorPath;
 
-  //randomChoreDoorGenerator();
+  } else if (choreDoor === 2){  
+    openDoor3 = botDoorPath;
+    openDoor2 = spaceDoorPath;
+    openDoor1 = beachDoorPath;
+  } // End if-else loop
 
-  */
+};  //randomChoreDoorGenerator
+
+
+/* function playDoor() decreases the numClosedDoors variable each time you click a door and checks if the game-winning condition (numClosedDoors === 0) has been met and if so, calls a gameOver() function
+*/
+
+
+
+randomChoreDoorGenerator();
+
+      
