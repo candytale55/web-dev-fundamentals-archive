7cdce2e96b3725a22cf9c0df20b56ee6cfc20af9
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2020-03-07 00:22:00 +0100

    Added URL paths in addition to relative paths. I finaly checked my Dev Tools and fixed my broken images problem. Added playDoor() and isClicked() code.

--- README.md
+++ README.md
@@ -5,3 +5,29 @@ JS, html & css single-page website with fully-functional game. Three doors, you 
 Third commit - Added html instructions section and CSS for styling. Re-did the function randomChoreDoorGenerator and the if-else loop inside it. 
 
 There's an ERROR : The img are not loading correctly (broken image). I already tested using the relative paths directly in randomChoreDoorGenerator and the results are the same. It works when the variable e.g. spaceDoorPath are used directly, but not when it is assigned inside randomChoreDoorGenerator. 
+
+2020-03.05 -  ERROR : The img are still not loading. I already tested using the relative paths directly in randomChoreDoorGenerator and the results are the same. It works when the variable e.g. spaceDoorPath are used directly, but not when it is assigned inside randomChoreDoorGenerator. I tried different syntaxis for the paths without any changes.
+
+Fourth Commit: 2020.03.07 - 
+I added the URL to image links to see if that was the issue - It also got broken images. 
+I finaly checked my Dev Tools. IT WAS A STUPID STUPID ERROR. I got:
+
+    script.js:57 Uncaught ReferenceError: numClosedDoor is not defined
+    at randomChoreDoorGenerator (script.js:57)
+    at script.js:106"
+
+    I had my variables with different names - just set all of them to numClosedDoors.
+
+    Next errors: 
+
+    script.js:104 Uncaught TypeError: Assignment to constant variable.
+    at playDoor (script.js:104)
+    at HTMLImageElement.doorImage2.onclick (script.js:59)
+
+    solved declaring numClosedDoors with let instead of const.
+
+    Error free !!! 
+
+I kept all the notes just in case. For the fifth commit I will have a clean-from-silly-error-notes version.
+
+Added URL paths in addition to relative paths. I finaly checked my Dev Tools and fixed my broken images problem. Added playDoor() and isClicked() code. 
--- script.js
+++ script.js
@@ -6,37 +6,73 @@ let doorImage1 = document.getElementById('door1');
 let doorImage2 = document.getElementById('door2');
 let doorImage3 = document.getElementById('door3');
 
-// variables containing a string with the ChoreBot, Beach and Space Door paths.
-const botDoorPath = "img/robot.svg"; 
-const beachDoorPath = "img/beach.svg";
-const spaceDoorPath = "img/space.svg";
+// variables containing a string with the project path for Closed, ChoreBot, Beach and Space Door paths.
+const botDoorPath = 'img/robot.svg'; 
+const beachDoorPath = 'img/beach.svg';
+const spaceDoorPath = 'img/space.svg';
+const closedDoorPath = 'img/closed_door.svg';
 
-const numClosedDoors = 3; // amount of doors in the game
+// Online images - to test the rest of the code while I solve the error of the broken images below.
+// IT ALSO GENERATES THE BROKEN IMAGES
+/*
+const botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg'; 
+const beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
+const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
+const closedDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg';
+*/
 
-let openDoor1, openDoor2, openDoor3 ; //They’ll be assigned values within randomChoreDoorGenerator()
+
+let numClosedDoors = 3; // amount of doors in the game
+
+let openDoor1, openDoor2, openDoor3; //They’ll be assigned values within randomChoreDoorGenerator()
 
 
 ///////////////////////////////////////////////////////////
 
 // doorImage1.onclick run when door images are clicked. 
 doorImage1.onclick = () => {
-  doorImage1.src = openDoor1;   // when you click on the door, the closed door changes to the ChoreBot or to the Space or Beach landscapes.
+  doorImage1.src = openDoor1;  // click on the door = img changes to a landscape (ChoreBot, Space Beach)
+  playDoor();
 };
+
 ///////// ERROR : The img are not loading. I already tested using the relative paths directly in randomChoreDoorGenerator and the results are the same. It works when the variable e.g. spaceDoorPath are used directly, but not when it is assigned inside randomChoreDoorGenerator. 
+// It isn't fixed by changing the relative path variables (const botDoorPath = 'img/robot.svg';)
+// It isn't fixed by adding '' ("'img/robot.svg'")
+// It isn't fixed by adding ./ to the path ("./img/robot.svg")
+// It isn't fixed by adding both ("'./img/space.svg'";)
+
+/* I finaly checked my Dev Tools. I got an "
+    script.js:57 Uncaught ReferenceError: numClosedDoor is not defined
+    at randomChoreDoorGenerator (script.js:57)
+    at script.js:106"
+
+    IT WAS A STUPID STUPID ERROR, I had my variables with different names - just set all of them to numClosedDoors.
+
+    Next error: 
+
+    script.js:104 Uncaught TypeError: Assignment to constant variable.
+    at playDoor (script.js:104)
+    at HTMLImageElement.doorImage2.onclick (script.js:59)
+
+    solved declaring numClosedDoors with let instead of const.
+
+    Error free !!! 
+*/
+
 doorImage2.onclick = () => {
-  doorImage2.src= spaceDoorPath;  
+  doorImage2.src= openDoor2;
+  playDoor();  
 };
 
 doorImage3.onclick = () => {
   doorImage3.src = openDoor3;
+  playDoor();
 };
 
-
-
   
-  // generates the door that hides the ChoreBot.
-const randomChoreDoorGenerator = () => {
-  const choreDoor = Math.floor(Math.random() * numClosedDoor); // Will randomly generate a whole number between 0 and 2. 
+  // generates the door that hides the ChoreBot randomly.
+  const randomChoreDoorGenerator = () => {
+  const choreDoor = Math.floor(Math.random() * numClosedDoors); // Will randomly generate a whole number between 0 and 2. 
 
   if (choreDoor === 0) {
     openDoor1 = botDoorPath;
@@ -57,8 +93,31 @@ const randomChoreDoorGenerator = () => {
 };  //randomChoreDoorGenerator
 
 
+/* function isClicked() prevents players to click the same door (even if it’s opened) multiple times to decrease the numClosedDoors value down to 0 and cheat to become a winner. It returns a boolean value after comparing if path is the same as Closed_Door */
+const isClicked = (door) => {
+  if (door.src === closedDoorPath){
+    return true;
+  } else {
+    return false;
+  }
+} // isClicked function
+
+
 /* function playDoor() decreases the numClosedDoors variable each time you click a door and checks if the game-winning condition (numClosedDoors === 0) has been met and if so, calls a gameOver() function
 */
+const playDoor = () => {
+  numClosedDoors--;
+
+  if (numClosedDoors === 0){ // Winning condition
+    gameOver();
+   }
+} // playDoor 
+
+
+
+const gameOver = () => {
+
+}
 
 
 
