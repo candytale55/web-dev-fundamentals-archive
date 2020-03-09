3daf5da3d294d70f2204ee000f99012e44e401a2
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2020-03-09 21:56:32 +0100

    Update filter_examples.js

--- filter_examples.js
+++ filter_examples.js
@@ -1,4 +1,6 @@
-/* The method .filter() returns a new array after filtering out elements from the original array. The callback function should return true or false depending on the element passed to it. Those  elements that the callback function return true are added to the new array. */
+/* The method .filter() returns a new array after filtering out elements from the original array. 
+The callback function should return true or false depending on the element passed to it. 
+Those  elements that the callback function return true are added to the new array. */
 
 const words = ['amusing', 'serendipity', 'oak', 'forest', 'chamaeleon', 'fly', 'eternity', 'biscuit'];
 
@@ -44,3 +46,16 @@ const longFavoriteWords = favoriteWords.filter(
   }
 );
 console.log(longFavoriteWords); // [ 'nostalgia', 'hyperbole', 'esoteric' ]
+
+
+
+
+
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+// return a new array with only those elements longer than 7 characters.
+const longCities = cities.filter(city => city.length > 7);
+console.log("Cities with long names: " + longCities); /* Prints:
+Cities with long names: Edinburgh,Eskisehir,Medellin,Yokohama
+*/
+
