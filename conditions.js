53f7290c75bc00fb5dccf3719b142f35a723f006
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2020-04-04 02:42:07 +0200

    Update conditions.js

--- conditions.js
+++ conditions.js
@@ -39,3 +39,38 @@ if(newSale) {
 }
 
 
+
+// ELSE ... IF Statements
+/*
+Allow different possible outcomes. The first condition that evaluates true from TOP to BOTTOM gets executed.
+
+The else if always comes after the if statement and before an else statement. */
+
+let stopLight = 'yellow';
+
+if (stopLight === 'red') {
+  console.log('Stop!');
+} else if (stopLight === 'yellow') {
+  console.log('Slow down.');
+} else if (stopLight === 'green') {
+  console.log('Go!');
+} else {
+  console.log('Caution, unknown!');
+}
+
+
+// Program that keeps track of the way the environment changes with the seasons.
+
+let season = 'summer';
+
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+} else if (season === 'winter') {
+  console.log('It\'s winter! Everything is covered in snow.');
+} else if(season === 'fall'){
+  console.log('It\'s fall! Leaves are falling!');
+} else if (season === 'summer'){
+  console.log('It\'s sunny and warm because it\'s summer!');
+} else {
+  console.log('Invalid season.');
+}
