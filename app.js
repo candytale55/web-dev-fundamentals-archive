b6512f73af11356a0e487298020e98893c3400ac
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2020-05-04 19:27:37 +0200

    Create app.js
    Not working file.

---
+++ app.js
@@ -0,0 +1,8 @@
+import * as country from './countrycreator.js';
+export {country};
+
+//const country = require('./countrycreator.js');
+
+const Mexico = new country("Mexico", "CDMX", "America");
+
+display(Mexico);
