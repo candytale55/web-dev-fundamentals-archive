7800081b4504d508e361f71d32109f116056b7b1
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2020-05-07 23:01:12 +0200

    Created methods isDead and wasBorn and set the index.html elements to display info from person objects - First test subject is Anne Bonny

--- main.js
+++ main.js
@@ -33,6 +33,19 @@ class Person{
         return this._POB;
     }
 
+    isDead(date, place){
+        this._DOD = date;
+        if(place!==undefined){
+            this._POD = place;
+        }
+    }
+    get DOD(){
+        return this._DOD;
+    }
+    get POD(){
+        return this._POD;
+    }
+
     setimg(imageURI){
         this._img = imageURI;
     }
@@ -54,9 +67,35 @@ const AnneBonny = new Person("Anne Bonny", "female", "Irish", "Pirate");
 AnneBonny.setwikiURI("https://en.wikipedia.org/wiki/Anne_Bonny");
 AnneBonny.setimg("https://upload.wikimedia.org/wikipedia/commons/archive/b/be/20060312000312%21Bonney%2C_Anne_%281697-1720%29.jpg")
 AnneBonny.wasBorn("possibly 1697", "Kingdom of Ireland");
+AnneBonny.isDead("possibly April 1782", "unknown");
 
 
 const myPerson = AnneBonny;
 
+document.getElementById("name").innerHTML = myPerson.name;
+document.getElementById("CON").innerHTML = myPerson.CON;
+document.getElementById("gender").innerHTML = myPerson.gender;
+document.getElementById("category").innerHTML = myPerson.category;
 
-document.getElementById("show").innerHTML = JSON.stringify(myPerson);
+if (myPerson.DOB){
+    document.getElementById("DOB").innerHTML = myPerson.DOB;
+}
+if (myPerson.POB){
+    document.getElementById("POB").innerHTML = myPerson.POB;
+}
+
+if (myPerson.DOD){
+    document.getElementById("DOD").innerHTML = myPerson.DOD;
+}
+if (myPerson.POD){
+    document.getElementById("POD").innerHTML = myPerson.POD;
+}
+
+document.getElementById("portrait").src= myPerson.img;
+document.getElementById("portrait").alt = "Portrait of " + myPerson.name;
+
+document.getElementById("moreinfo").innerHTML = myPerson.name;
+document.getElementById("wikilink").href = myPerson.wikiURI;
+
+
+//document.getElementById("show").innerHTML = JSON.stringify(myPerson);
