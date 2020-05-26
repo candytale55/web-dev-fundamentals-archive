22f7995ca1c834c6c443e78ade5883e4449641ea
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2020-05-27 00:23:16 +0200

    created the Tetrominos

--- app.js
+++ app.js
@@ -5,6 +5,31 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelector('#score');
   const StartBtn = document.querySelector('#start-button');
   const width = 10;
-
 //  console.log(squares)
+
+  // TETROMINOES
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2 ],
+    [width, width*2, width*2+1, width*2+2]
+  ];
+
+
+
+
 })
+
+
+/*
+
+const lTetromino = [
+  [],[],[],[]
+];
+
+
+  function showAlert() {
+    alert('You have been alerted');
+  }
+  showAlert();
+*/
