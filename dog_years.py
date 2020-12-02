0deafbe2ff2f14f2353b31a34d72ad36c4b71622
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2020-12-02 21:28:45 +0100

    Create dog_years.py

---
+++ dog_years.py
@@ -0,0 +1,13 @@
+# Say that every one year of a human’s life is equivalent to seven years of a dog’s life.
+
+# Write your dog_years function here:
+def dog_years(name, age):
+  return  name +", you are "+ str(age*7)+" years old in dog years"
+
+
+# Uncomment these function calls to test your dog_years function:
+print(dog_years("Lola", 16))
+# should print "Lola, you are 112 years old in dog years"
+
+print(dog_years("Baby", 0))
+# should print "Baby, you are 0 years old in dog years"
