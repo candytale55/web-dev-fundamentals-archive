70524ab15e8e7646b2feaeb2928f3d31f70a78d6
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-01-20 22:36:05 +0100

    Added CD Class & cleaned code.
    Cleaned code: for example:
    
        if (this._isCheckedOut === false){
          this._isCheckedOut = true;
        } else {
          this._isCheckedOut = false;
        }
    
    Became:     
    this._isCheckedOut = !this._isCheckedOut;
    
    
    
    And . . .
    
    const reducer =  (accumulator, currentValue) => accumulator + currentValue;
        let average = this.ratings.reduce(reducer) / this.ratings.length
    
    
    became:
    
        let average = this.ratings.reduce((accumulator, rating) => accumulator + rating); 
        return average / this.ratings.length;
      }

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ class Media {
     this._isCheckedOut = false;
     this._ratings = [];
   }
-  // GETTERS:
+  
   get title(){
     return this._title;
   }
@@ -15,20 +15,11 @@ class Media {
     return this._ratings;
   }
 
-  // METHODS:
   toggleCheckOutStatus(){
-     /*if (this._isCheckedOut === false){
-      this._isCheckedOut = true;
-    } else {
-      this._isCheckedOut = false;
-    }*/
     this._isCheckedOut = !this._isCheckedOut;
   }
   
   getAverageRating(){
- /*  const reducer =  (accumulator, currentValue) => accumulator + currentValue;
-    let average = this.ratings.reduce(reducer) / this.ratings.length;
-*/
     let average = this.ratings.reduce((accumulator, rating) => accumulator + rating); 
     return average / this.ratings.length;
   } 
@@ -37,6 +28,8 @@ class Media {
     this.ratings.push(newRating);
   }
 }
+
+
 
 
 
@@ -57,6 +50,9 @@ class Book extends Media {
 }
 
 
+
+
+
 class Movie extends Media {
   constructor(title, director, runTime){
     super(title);
@@ -73,8 +69,50 @@ class Movie extends Media {
 
 
 
+class CD extends Media {
+  constructor(title, artist, length){
+    super(title);
+    this._artist = artist;
+    this._length = length;
+    this._songTitles = [];
+  }
+  get artist(){
+    return this._artist;
+  }
+  get length(){
+    return this._length;
+  }
+  get songTitles(){
+    return this._songTitles;
+  }
 
-// TESTS
+  addSongList(arrayOfSongs){
+    this._songTitles = arrayOfSongs;
+  }
+
+ shuffle(array) {
+  let shuffledArray = array;
+  var currentIndex = shuffledArray.length, temporaryValue, randomIndex; 
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = shuffledArray[currentIndex];
+    shuffledArray[currentIndex] = shuffledArray[randomIndex];
+    shuffledArray[randomIndex] = temporaryValue;
+  }
+   return shuffledArray;
+ } // End shuffle
+
+} // End Class CD
+
+
+
+
+
+
+
+// TESTS:
+
 const historyOfEverything = new Book ('A Short History of Nearly Everything', 'Bill Bryson', 544)
 console.log(historyOfEverything);
 historyOfEverything.toggleCheckOutStatus();
@@ -84,14 +122,42 @@ historyOfEverything.addRating(5);
 historyOfEverything.addRating(5);
 console.log(historyOfEverything.getAverageRating());
 
+
 const speed = new Movie ('Speed', 'Jan de Bont', 116)
 console.log(speed);
 speed.toggleCheckOutStatus();
-//speed.toggleCheckOutStatus();
+speed.toggleCheckOutStatus();
 speed.addRating(1);
 speed.addRating(1);
 speed.addRating(5);
 console.log(speed);
 console.log(speed.getAverageRating());
 
+
+const appetite = new CD("Appetite for Destruction", "Guns n' Roses", 54)
+appetite.addRating(4);
+appetite.addRating(5);
+appetite.addRating(3);
+console.log(appetite.getAverageRating());
+appetite.addSongList(
+  [
+  "Welcome to the Jungle",
+  "It's So Easy",
+  "Nightrain",
+  "Out ta Get Me",
+  "Mr. Brownstone",
+  "Paradise City",
+	"My Michelle",
+  "Think About You",
+  "Sweet Child o' Mine",
+  "You're Crazy",
+  "Anything Goes",
+  "Rocket Queen"]
+);
+//console.log(appetite);
+
+console.log(appetite.shuffle(appetite.songTitles))
+
+
 // For reference: https://www.youtube.com/watch?v=_7HStd1Vhlc
+
