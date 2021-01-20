a76ae74fb15c874c28006e7d049652782c5dbdb0
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-01-20 22:00:33 +0100

    Create shuffle.js

---
+++ shuffle.js
@@ -0,0 +1,29 @@
+// JavaScript ES6
+
+
+/* Used Fisher-Yates (aka Knuth) Shuffle algorithm found here:
+https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+http://sedition.com/perl/javascript-fy.html */
+
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// Used like so
+var arr = [2, 11, 37, 42];
+shuffle(arr);
+console.log(arr);
