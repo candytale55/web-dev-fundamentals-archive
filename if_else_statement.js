f3d8bd4184dc1c5a0cfa208dbfa0e9ba571467f9
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-02-07 21:44:24 +0100

    If Else Statement example

---
+++ if_else_statement.js
@@ -0,0 +1,23 @@
+/*
+We can add an "else" to add a default behavior - to execute certain code if the condition is false.
+*/
+if (false) {
+  console.log('This block will not run.');
+} else {
+  console.log('But this code will because the condition evaluates to true!');
+}
+
+
+
+// IF ... ELSE Statements
+/*To run different code if a condition is met, and adding more conditions if not.*/
+
+let newSale = true;
+newSale = false;
+
+if(newSale) {
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.');
+}
+
