74cbefe5adb12888ec5e9f133eb35b2e49e68ca5
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-02-10 21:55:50 +0100

    calling a function

---
+++ say_thanks.js
@@ -0,0 +1,13 @@
+
+// You manage an online store. When a customer places an order, you send them a thank you note. 
+
+function sayThanks() {
+  console.log('Thank you for your purchase! We appreciate your business.')
+}
+
+sayThanks();
+
+sayThanks();
+
+sayThanks();
+
