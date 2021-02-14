c4a0a7bf3276f65453e0207d5a3188f5f502b316
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-02-14 21:16:15 +0100

    Examples updating array elements.
    The line, seasons[3] = 'Autumn'; tells our program to change the item at index 3 of the seasons array to be 'Autumn' instead of what is already there.

---
+++ 04_seasons.js
@@ -0,0 +1,17 @@
+// Update Elements
+
+let seasons = ['Winter', 'Spring', 'Summer', 'Fall'];
+
+seasons[3] = 'Autumn';
+console.log(seasons);  //Output: ['Winter', 'Spring', 'Summer', 'Autumn']
+
+/*
+The line, seasons[3] = 'Autumn'; tells our program to change the item at index 3 of the seasons array to be 'Autumn' instead of what is already there.
+*/
+
+let groceryList = ['bread', 'tomatoes', 'milk'];
+console.log(groceryList);
+
+groceryList[1]= 'avocados';
+console.log(groceryList);
+
