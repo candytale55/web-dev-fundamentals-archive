f56d0f9c4dacb63a73d0c53f982a24a95fba0a51
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-04-01 15:17:36 +0200

    Update factorialize.js

--- factorialize.js
+++ factorialize.js
@@ -199,8 +199,22 @@ console.log(factorialize_setting_an_array(0));  // 1
 
 
 
-////// RECURSIVE SOLUTION
+////// RECURSIVE SOLUTIONS
 
+/*
+In traditional head recursion, you perform your recursive calls first, 
+and then you take the return value of the recursive call and calculate the result. 
+In this manner, you don’t get the result of your calculation 
+until you have returned from every recursive call.
+
+In tail recursion, you perform your calculations first, 
+and then you execute the recursive call, passing the results 
+of your current step to the next recursive step. 
+This results in the last statement being in the form of 
+(return (recursive-function params)).
+*/
+
+// HEAD RECURSION
 function factorialize_recursion(num) {
   if (num === 0){
     return 1;
@@ -220,3 +234,33 @@ console.log(factorialize_recursion(3));  // 6
 console.log(factorialize_recursion(5));  // 120
 console.log(factorialize_recursion(10)); // 3628800
 console.log(factorialize_recursion(20)); // 2432902008176640000
+
+
+
+
+// TAIL RECURSION
+
+/*
+In this solution, with each evaluation of the recursive call, the factorial is updated. 
+This is different from the head-recursive solution where all evaluation calculations 
+are stored on the stack until the base case is reached.
+*/
+
+
+function factorialize_tail_recursion(num, factorial = 1) {
+  if (num === 0){
+    return factorial; // base case
+  } else {
+    return factorialize(num-1, factorial * num);
+  }   
+}
+
+
+
+// TESTS
+console.log(factorialize_tail_recursion(1));  // 1
+console.log(factorialize_tail_recursion(0));  // 1
+console.log(factorialize_tail_recursion(3));  // 6
+console.log(factorialize_tail_recursion(5));  // 120
+console.log(factorialize_tail_recursion(10)); // 3628800
+console.log(factorialize_tail_recursion(20)); // 2432902008176640000
