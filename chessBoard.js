d09719a19683f85c5d94aafab1cd39efdef48e2d
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-04-02 17:26:12 +0200

    Update chessBoard.js

--- chessBoard.js
+++ chessBoard.js
@@ -14,3 +14,24 @@ for (let i=0; i<=size; i++) {
   }
   console.log(pattern);
 }
+
+
+
+
+// Your code here.
+let size = 8;
+let pattern = "";
+
+for (let i=0; i<=size; i++) {  
+  if (i % 2 === 0){
+       pattern = " #";
+   } else {
+       pattern = "# ";
+   }  
+  for (let j=0; j<=size/2; j++){
+  	pattern += pattern;
+  }
+  
+  pattern += "\n";
+  console.log(pattern);
+}
