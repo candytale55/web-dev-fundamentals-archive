f3692f506ab8fc3b04bfb4d251f33e267c9d187a
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-04-03 14:37:08 +0200

    Update findLongestWordLength.js

--- findLongestWordLength.js
+++ findLongestWordLength.js
@@ -1,9 +1,9 @@
 // Returns the length of the longest word in string
 
-/* Return the length of the longest word in the provided sentence.*/
 
 
 
+/* Return the length of the longest word in the provided sentence. This keeps the word and returns its length*/
 function findLongestWordLength_Arr_For_loop(str) {
   let longest = "";
   let array = str.split(" ");
@@ -21,3 +21,28 @@ console.log(findLongestWordLength_Arr_For_loop("May the force be with you")); //
 console.log(findLongestWordLength_Arr_For_loop("Google do a barrel roll")); // 6
 console.log(findLongestWordLength_Arr_For_loop("What is the average airspeed velocity of an unladen swallow")); // 8
 console.log(findLongestWordLength_Arr_For_loop("What if we try a super-long word such as otorhinolaryngology")); // 19
+
+
+
+
+
+
+/* Returns only the length of the longest word in the string. */
+
+function findLongestWordLength_split_arr(str) {
+  let array = str.split(" ");
+  let longest = 0;
+  for (let i=0; i<array.length; i++){
+    if (array[i].length >= longest){
+      longest = array[i].length;
+    }
+  }
+  return longest;
+}
+
+// TESTS: 
+console.log(findLongestWordLength_split_arr("The quick brown fox jumped over the lazy dog")); //6
+console.log(findLongestWordLength_split_arr("May the force be with you")); // 5
+console.log(findLongestWordLength_split_arr("Google do a barrel roll")); // 6
+console.log(findLongestWordLength_split_arr("What is the average airspeed velocity of an unladen swallow")); // 8
+console.log(findLongestWordLength_split_arr("What if we try a super-long word such as otorhinolaryngology")); // 19
