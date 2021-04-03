37156992662e90f1b6618c5e1267dcdcd00a123b
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-04-03 14:52:43 +0200

    Update findLongestWordLength.js

--- findLongestWordLength.js
+++ findLongestWordLength.js
@@ -46,3 +46,44 @@ console.log(findLongestWordLength_split_arr("May the force be with you")); // 5
 console.log(findLongestWordLength_split_arr("Google do a barrel roll")); // 6
 console.log(findLongestWordLength_split_arr("What is the average airspeed velocity of an unladen swallow")); // 8
 console.log(findLongestWordLength_split_arr("What if we try a super-long word such as otorhinolaryngology")); // 19
+
+
+
+
+
+
+
+
+
+/* 
+USING REDUCE and Math.max: 
+
+built-in .reduce() method iterates through an array and returns a single value.  
+It takes a callback function with two parameters (accumulator, currentValue) as arguments. 
+
+On each iteration, accumulator is the value returned by the last iteration, 
+and the currentValue is the current element. 
+Optionally, a second argument can be passed which acts as the initial value of the accumulator.
+
+Math.max() function returns the largest of the zero or more numbers given as input parameters, 
+or NaN if any parameter isn't a number and can't be converted into one.
+
+In this function, the 0 after the callback function is used to give an initial value 
+to the longest, so that Math.max will know where to start. 
+longestSoFar is the accumulator and word is the currentValue;
+*/
+
+function findLongestWordLength_reduce_MathMax(str) {
+  let longest = str.split(" ").reduce(function(longestSoFar, word){
+  return Math.max(longestSoFar, word.length)
+}, 0);
+  return longest; 
+}
+
+// TESTS: 
+console.log(findLongestWordLength_reduce_MathMax("The quick brown fox jumped over the lazy dog")); //6
+console.log(findLongestWordLength_reduce_MathMax("May the force be with you")); // 5
+console.log(findLongestWordLength_reduce_MathMax("Google do a barrel roll")); // 6
+console.log(findLongestWordLength_reduce_MathMax("What is the average airspeed velocity of an unladen swallow")); // 8
+console.log(findLongestWordLength_reduce_MathMax("What if we try a super-long word such as otorhinolaryngology")); // 19
+
