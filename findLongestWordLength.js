1f76c120b51efa0a5d9356a48103b2bef63e1cc5
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-04-03 15:31:26 +0200

    Update findLongestWordLength.js

--- findLongestWordLength.js
+++ findLongestWordLength.js
@@ -146,3 +146,38 @@ console.log(findLongestWordLength_Map_MathMax_Spread_Simpler("May the force be w
 console.log(findLongestWordLength_Map_MathMax_Spread_Simpler("Google do a barrel roll")); // 6
 console.log(findLongestWordLength_Map_MathMax_Spread_Simpler("What is the average airspeed velocity of an unladen swallow")); // 8
 console.log(findLongestWordLength_Map_MathMax_Spread_Simpler("What if we try a super-long word such as otorhinolaryngology")); // 19
+
+
+
+
+
+/* USING RECURSION 
+An array words contains the words splitted from str. 
+
+We do recursion: base case is When words only has 1 element left. If so, then that is the longest element and we return it. 
+
+Otherwise, we remove the first element and recursively call the function, returning the maximum between the length of the first result and the recursive call 
+*/
+
+
+function findLongestWordLength_Rec(str) {
+  const words = str.split(" ");
+  //console.log(words);
+  // base case: There's only one element left that is the longest (although it could be a tie - let's not think that way now) 
+  if (words.length === 1){
+    return words[0].length;
+  }
+
+  /* Use recursive call to call this function on words array */  
+  return Math.max(words[0].length, findLongestWordLength_Rec(words.slice(1).join(" "))
+  );
+}
+
+
+
+// TESTS: 
+console.log(findLongestWordLength_Rec("The quick brown fox jumped over the lazy dog")); //6
+console.log(findLongestWordLength_Rec("May the force be with you")); // 5
+console.log(findLongestWordLength_Rec("Google do a barrel roll")); // 6
+console.log(findLongestWordLength_Rec("What is the average airspeed velocity of an unladen swallow")); // 8
+console.log(findLongestWordLength_Rec("What if we try a super-long word such as otorhinolaryngology")); // 19
