e62bd66e8bdbabe1b541b6573cacfad97572663c
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-04-04 10:07:36 +0200

    Capitalize using replace and arrays
    Simplified version

--- titleCase_usingReplace.js
+++ titleCase_usingReplace.js
@@ -1,22 +1,22 @@
 /*  
 USING REPLACE
-Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.  
+Return the provided string with the first letter of each word capitalized. 
+Make sure the rest of the word is in lower case.  
 For the purpose of this exercise, you should also capitalize connecting words like the and of.
 */
+
 
 
 function titleCase(str) {
   str = str.toLowerCase().split(" ");
   let newStrArray = [];
 
-  for (let i=0; i<=str.length-1; i++){
-    let newWord = "";
-    newWord = str[i].replace(str[i][0], str[i][0].toUpperCase())
+  for (var i in str){
+    let newWord = str[i].replace(str[i][0], str[i][0].toUpperCase())
     newStrArray.push(newWord);
   }
   return newStrArray.join(" ");
 }
-
 
 
 
@@ -33,3 +33,6 @@ console.log(titleCase("Anita lava la tina"));
 // Anita Lava La Tina
 console.log(titleCase("piña PIÑA piña, PARA la NIÑA")); 
 // Piña Piña Piña, Para La Niña
+
+
+
