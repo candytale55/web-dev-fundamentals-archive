35238c2798ee66e8f16ce8611d656b46c775162a
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-04-09 12:47:57 +0200

    Repeats a given string str  for num times

--- repeatStringNumTimes.js
+++ repeatStringNumTimes.js
@@ -17,3 +17,86 @@ function repeatStringNumTimes(str, num) {
   } 
   return newStr
 }
+
+
+
+// Using REPEAT:
+
+function repeatStringNumTimes(str, num) {
+  if (num < 0){
+    return ""
+  }
+  return str.repeat(num);
+}
+
+
+
+
+
+
+// Using CONCAT and a While loop
+
+function repeatStringNumTimes(str, num) {
+  if (num <= 0) {
+    return "empty";
+    }
+  let fragm = str;
+  let i=1;
+  while (i <= num-1){
+    str = str.concat(fragm);
+    i++;
+  }
+  return str;
+}
+
+
+
+// Using Recursion
+
+
+function repeatStringNumTimes(str, num) {
+  if (num < 1) {
+    return "";
+    } 
+  else {
+    return str + repeatStringNumTimes(str, num-1);
+  }
+}
+
+
+
+
+
+
+
+// Using Recursion and Ternary Operator
+
+function repeatStringNumTimes(str, num) {
+  return (num < 1) ? "" : str + repeatStringNumTimes(str, num-1)
+}
+
+
+
+
+// TESTS: 
+console.log(repeatStringNumTimes("abc", 3));
+//abcabcabc
+
+console.log(repeatStringNumTimes("abc", 4));
+// abcabcabcabc
+
+console.log(repeatStringNumTimes("abc", 1));
+// abc
+
+console.log(repeatStringNumTimes("*", 8));
+// ********
+
+console.log(repeatStringNumTimes("abc", -2));
+// empty
+
+console.log(repeatStringNumTimes("abc", 0));
+// empty
+
+console.log(repeatStringNumTimes("abccba", 3)); 
+// abccbaabccbaabccba
+
