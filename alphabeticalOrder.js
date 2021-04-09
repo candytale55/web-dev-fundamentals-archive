b1249f006858f43496638f737cb80ccda2600dff
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-04-09 08:25:33 +0200

    Sort in alphabetical order
    To sort in inverse alphabetical just change the  gt sign to lt sign:  return a === b ? 0: a < b ? 1 : -1;

--- alphabeticalOrder.js
+++ alphabeticalOrder.js
@@ -12,46 +12,7 @@ If compareFunction(a,b) returns a value greater than 0 for two elements a and b,
 If compareFunction(a,b) returns a value equal to 0 for two elements a and b, then a and b will remain unchanged.
 */
 
-
-function ascendingOrder(arr) {
-  return arr.sort(function(a, b) {
-    return a - b;
-  });
-}
-//console.log(ascendingOrder([1, 5, 2, 3, 4]));
-// [1, 2, 3, 4, 5].
-
-
-
-function reverseAlpha(arr) {
-  return arr.sort(function(a, b) {
-    return a === b ? 0 : a < b ? 1 : -1;
-  });
-}
-//console.log(reverseAlpha(['l', 'h', 'z', 'b', 's']));
-// ['z', 's', 'l', 'h', 'b'].
-
-
-function orderStuff(arr){
-  return arr.sort(
-    function (a,b) {
-      return a > b ? 0 : a < b ? 1 : -1;
-    }
-  );
-}
-//console.log(orderStuff([7, '/', ')', 'a', 0, '?']));
-// [ 0, '/', ')', 'a', '?', 7 ]
-
-
-/*
-Use the sort method in the alphabeticalOrder function to sort the elements of arr in alphabetical order.
-*/
-
-
-
-
-
-
+ 
 function alphabeticalOrder(arr) {
   return arr.sort(
     function (a,b) {
