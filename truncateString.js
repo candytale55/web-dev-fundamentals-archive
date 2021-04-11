5a283acf7cf1ced63036d2e3823560cbb21590cc
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-04-11 19:23:50 +0200

    Truncates a String at the lenght given by a number
    Truncate a string (first argument) if it is longer than the given maximum string length (second argument).  Return the truncated string with a ... ending.
    Examples with .slice() and .substring(). Using if-else clause and ternary operator

--- truncateString.js
+++ truncateString.js
@@ -16,6 +16,29 @@ function truncateString(str, num) {
 
 
 
+
+// Using substring
+
+function truncateString(str, num) {
+  if (str.length <= num){
+    return str;
+  } 
+  else {
+    return str.substring(0,num) +"...";
+  }
+}
+
+
+
+
+
+// Using substring (or slice, just substitute it) and ternary operator
+
+function truncateString(str, num) {
+  return str.length <= num ? str : str.substring(0,num) +"...";
+  }
+
+
 // TESTS:
 console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
 // A-tisket...
