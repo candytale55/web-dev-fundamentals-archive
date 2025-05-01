7a4673e193ffb1ab6a854533bc959d0a25465dfa
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2025-05-01 09:54:59 +0200

    Create build-a-javascript-trivia-bot.js
    Revisiting FCC exercises

---
+++ 2025/JS/build-a-javascript-trivia-bot.js
@@ -0,0 +1,16 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName ="Indentron the Python Fanbot";
+let botLocation="The Snakepit Sandbox";
+let favoriteLanguage="Python";
+console.log("My name is "+ botName +" and I live on " + botLocation + ".")
+console.log("My favorite programming language is "+ favoriteLanguage +".")
+//first codingFact
+let codingFact= favoriteLanguage + " is named after Monty Python, not the snake. So when you're stuck on a bug, remember—it's all just a very silly sketch gone wrong.";
+console.log(codingFact);
+//second codingFact
+codingFact= "Fun"+ favoriteLanguage +"fact: You can swap variables with `a, b = b, a` and feel like a wizard.";
+console.log(codingFact);
+//third codingFact
+codingFact="Fun "+ favoriteLanguage +"fact: In "+ favoriteLanguage +", everything is an object—even functions! You can pass them around like variables and call them like they're just another variable. Try that with JavaScript's function keyword! Hahaha";
+console.log(codingFact)
+console.log("It was fun sharing these facts with you. Goodbye! - "+ botName +" from "+ botLocation +".");
