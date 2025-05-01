f196795348b78e281319e35d473d107b3b20ecf9
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2025-05-01 10:54:42 +0200

    Create build-a-sentence-maker.js
    Revisiting FCC examples

---
+++ 2025/JS/build-a-sentence-maker.js
@@ -0,0 +1,25 @@
+// Create two different stories with a sentence template and output the stories to the console using variables.
+
+let adjective = "spiky";
+let noun = "llama";
+let noun2 = "cheeseburgers";
+let place = "bouncy castle";
+let adjective2 = "glittery";
+let verb = "sneezing";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun +" who loved to eat " + noun2 +". The " + noun +" lived in a " + place +" and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+ 
+let secondStory;
+
+adjective = "grumpy";
+noun = "toaster";
+noun2 = "marshmallows";
+place = "haunted igloo";
+adjective2 = "rainbow-colored";
+verb = "dancing";
+
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun +" who loved to eat " + noun2 +". The " + noun +" lived in a " + place +" and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
