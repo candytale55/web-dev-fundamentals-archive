924d37c632f70734ed537b30c4428594af6f8eca
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2025-05-04 12:58:28 +0200

    Update build-a-pyramid-generator.js
    Finished. Excercise from: Exercise from: // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/#learn-introductory-javascript-by-building-a-pyramid-generator

--- 2025/JS/build-a-pyramid-generator.js
+++ 2025/JS/build-a-pyramid-generator.js
@@ -54,12 +54,64 @@ while (rows.length < count) {
   rows.push(padRow(rows.length + 1, count));
 }
 
-// ------------------------------ UPSIDE DOWN PYRAMID --------------------------------
+// --------------- - UPSIDE DOWN PYRAMID WITH BACKWARDS LOOP ------------------------------
 
-// Steps 100 onward
+// Steps 100 - 105
 // conditions that also work for the iteration:  i = i-1, i -= 1, 
 
 for (let i = count; i > 0; i--) {
   rows.push(padRow(i, count));  // the loop needs to run while i is greater than 0.
 }
+
+
+// -------------- UPSIDE DOWN PYRAMID WITH unshift() ----------------------------------
+
+// Step 111 
+for (let i = 1; i <= count; i++) {
+  rows.unshift(padRow(i, count));  // To make it regular, just use .push() instead of .unshift()
+}
+
+
+// ------------------------ TOGGLE THE PYRAMID ----------------------------------
+// Steps 116
+
+let inverted = true;
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+// -------------------- END PYRAMID - TOGGLE WITH ! --------------
+// Cleaned
+
+const character = "!";
+const count = 10;
+const rows = [];
+let inverted = false;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
+
+
 
