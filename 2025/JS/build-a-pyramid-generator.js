5f67012a4d4837c689ddeb3a4c3595ef7cb1bb6e
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2025-05-04 09:34:21 +0200

    Update build-a-pyramid-generator.js

--- 2025/JS/build-a-pyramid-generator.js
+++ 2025/JS/build-a-pyramid-generator.js
@@ -7,10 +7,20 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i = i + 1) {
+// i++, i+= 1, i = i +1  work the same  
+for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}
+// Check 70, 75 on 
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/#learn-introductory-javascript-by-building-a-pyramid-generator
+
+/*
+// Does the same, check 75 i <=:
+for (let i = 0; i < count; i ++) { 
   rows.push(padRow(i + 1, count));
 }
-// Check around 70 on https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/#learn-introductory-javascript-by-building-a-pyramid-generator
+*/
+
 
 let result = ""
 
