d596670ca83777366ff99515f3248e9789fd11c8
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2025-05-07 12:45:20 +0200

    Update build-a-gradbook.js

--- 2025/JS/build-a-gradbook.js
+++ 2025/JS/build-a-gradbook.js
@@ -1,3 +1,62 @@
+
+// OLD VERSION
+
+// ! Reduce() worked, but the exercise required a for loop.  
+function getAverage(scores) {
+  let sum = 0;  
+  for (const score of scores) {
+    sum += score;
+  }
+  return sum / scores.length;
+}
+
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
+// Returns boolean. True if the score is different than "F", false if it isn't.
+function hasPassingGrade(score) {
+  return getGrade(score) !== "F";
+}
+
+// The average is for the total scores of all the students. The Grade is just for the individual student.
+// ! The problem required to use concatenation for the messages.
+function studentMsg(totalScores, studentScore) {
+  if(hasPassingGrade(studentScore)){
+    return "Class average: " + getAverage(totalScores) + ". Your grade: " + getGrade(studentScore) + ". You passed the course.";
+  } else{
+    return "Class average: " + getAverage(totalScores) + ". Your grade: " + getGrade(studentScore) + ". You failed the course.";
+  }
+}
+
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 97));
+// "Class average: 71.7. Your grade: A. You passed the course.""
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 42));
+//Class average: 71.7. Your grade: F. You failed the course.
+
+
+
+
+
+
+
+
+
+
+// NEW VERSION - 
+
 function getAverage(arr){
   return arr.reduce(
   (accumulator, currentValue) => accumulator + currentValue, 0,) / arr.length;
