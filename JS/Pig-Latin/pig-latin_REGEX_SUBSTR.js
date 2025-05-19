5ac8ab2b06a54f6a74dc376f83b9a71f97af05d3
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2025-05-19 19:18:36 +0200

    Update and rename Pig-Latin/JSpig-latin_REGEX_SUBSTR.js to JS/Pig-Latin/pig-latin_REGEX_SUBSTR.js

--- JS/Pig-Latin/pig-latin_REGEX_SUBSTR.js
+++ JS/Pig-Latin/pig-latin_REGEX_SUBSTR.js
@@ -1,4 +1,4 @@
-/*
+
 Pig Latin is a way of altering English Words. The rules are as follows:
 
 - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
