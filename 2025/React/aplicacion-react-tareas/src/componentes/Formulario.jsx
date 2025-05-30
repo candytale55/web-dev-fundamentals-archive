d1fb62959642bc5a80475626e549a472be926646
CandyTale55 <candytale55@gmail.com>
2025-05-30 10:14:19 +0200

    Hasta [200~setInput(e.target.value); en Formulario

--- 2025/React/aplicacion-react-tareas/src/componentes/Formulario.jsx
+++ 2025/React/aplicacion-react-tareas/src/componentes/Formulario.jsx
@@ -1,8 +1,23 @@
 import '../hojas-de-estilo/Formulario.css'
-
+import React, {useState} from 'react';
 
 function Formulario(props) {
-    
+
+    const [input, setInput] = useState('');
+
+    const manejarCambio = e => { 
+        setInput(e.target.value);
+        console.log(e.target.value);
+
+    };
+
+    const manejarEnvio = e => { 
+        const tareaNueva = {
+            id: '34545',
+            texto: 'Hola'
+        }
+    };
+
     return (
         <form className='tarea-formulario'>
             <input
@@ -10,8 +25,10 @@ function Formulario(props) {
                 type='text'
                 placeholder='Escribe una tarea'
                 name='texto'
+                onChange={manejarCambio}
             />
-            <button className='tarea-boton'>Agregar tarea</button>
+            <button className='tarea-boton'>
+                Agregar tarea</button>
         </form>
     );
 }
--- 2025/React/aplicacion-react-tareas/src/componentes/ListaDeTareas.jsx
+++ 2025/React/aplicacion-react-tareas/src/componentes/ListaDeTareas.jsx
@@ -1,16 +1,30 @@
 import React, {useState} from 'react';
 import '../hojas-de-estilo/ListaDeTareas.css'
 import Formulario from '../componentes/Formulario';
+import Tarea from '../componentes/Tarea';
 
 
 function ListaDeTareas() {
+
   const [tareas, setTareas] = useState([]);
+  
+  const agregarTarea = tarea => {
+    console.log('Tarea Agregada')
+    console.log(tarea)
+  }
+
   return (
     <>
       <Formulario />
       <div className='tareas-lista-contenedor'>
         {
-          
+          tareas.map(
+            (tarea) => 
+              <Tarea
+                texto={tarea.texto}
+                completada={tarea.completada}
+              />
+          )
         }
       </div>
     </>
