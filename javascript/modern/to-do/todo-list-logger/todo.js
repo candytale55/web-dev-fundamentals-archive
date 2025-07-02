7d222b6d6a3a6178b21f5c86077190fea2433290
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2025-07-02 19:28:18 +0200

    feat: finished todo-list-logger in /javascript/modern/to-do /todo-list-logger/

---
+++ javascript/modern/to-do/todo-list-logger/todo.js
@@ -0,0 +1,39 @@
+let todoList = [];
+
+const addTask = (task) => {
+    todoList.push(task);
+    console.log(`Task "${task}" added. You now have ${todoList.length} tasks.`)
+}
+
+const removeTask = (task) => {
+    if (todoList.includes(task)) {
+        todoList.splice(todoList.findIndex((e) => e === task), 1);
+        console.log(`Task "${task}" removed. ${todoList.length} tasks remaining`);
+    }
+    else {
+        console.log(`Task "${task}" not found.`);
+    }
+} 
+
+
+
+// All tasks are case-sensitive (no normalization).
+// Do not log anything except what's specified.
+// <N> must be dynamically calculated.
+
+
+
+// TESTS: Test it by calling the functions with the examples
+addTask("Walk the dog");
+// logs: Task "Walk the dog" added. You now have 1 tasks.
+
+addTask("Buy milk");
+// logs: Task "Buy milk" added. You now have 2 tasks.
+
+removeTask("Buy milk");
+// logs: Task "Buy milk" removed. 1 tasks remaining.
+
+removeTask("Do taxes");
+// logs: Task "Do taxes" not found.
+
+
