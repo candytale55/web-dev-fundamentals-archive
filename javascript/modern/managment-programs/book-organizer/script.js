d0ca63cbf1e9b33906b32015ee0a746a6e3b9ee3
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2025-07-03 16:41:08 +0200

    Naive solution to book-organizer/script in javascript/modern/management-programs/book-organizer

--- javascript/modern/managment-programs/book-organizer/script.js
+++ javascript/modern/managment-programs/book-organizer/script.js
@@ -53,19 +53,43 @@ const books = [
 
 
 const sortByYear = (book1, book2) => {
+  
   if (book1.releaseYear < book2.releaseYear){
     //should return -1 if the releaseYear of the first book is smaller than that of the second book.
+    return -1;
   }
   if (book1.releaseYear > book2.releaseYear){
     //should return 1 if the releaseYear of the first book is bigger than that of the second book.
+    return 1;
   }
   if (book1.releaseYear === book2.releaseYear){
     // should return 0 if both releaseYear values are equal.
+    return 0;
   }
 }
 
 
-const filteredBooks = (year) => {
+
+const filterBooksByYear = (year, array) => {
+  
+  return array
+    .filter((e)=> e.releaseYear >= year)
+    .sort((a,b)=>a.releaseYear - b.releaseYear);
+  
   // filter out books written after a certain year such as 1950 from the books array and save the filtered array in a new array named filteredBooks
   // You should sort the books in the filteredBooks array according to their releaseYear in ascending order. 
 };
+
+
+console.log(sortByYear(  {
+    title: "My Booky Wook",
+    authorName: "Russell Brand",
+    releaseYear: 2007,
+  }, {
+    title: "I Feel Bad About My Neck: And Other Thoughts on Being a Woman",
+    authorName: "Nora Ephron",
+    releaseYear: 2006,
+  }));
+
+const filteredBooks = filterBooksByYear(2000, books);
+console.log(filteredBooks);
